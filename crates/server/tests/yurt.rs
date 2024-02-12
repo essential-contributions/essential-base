2a775a33252261745471262654617d96fb740668
Tom <tomrgowan@gmail.com>
2024-02-12 16:22:46 +1100

    add transfer test that fails

--- crates/server/tests/yurt.rs
+++ crates/server/tests/yurt.rs
@@ -110,6 +110,93 @@ solve satisfy;
     assert_eq!(utility, 2);
 }
 
+#[test]
+fn test_erc20() {
+    // Transfer
+    let code = r#"
+let sender: int[4];
+let to: int[4];
+let amount: int;
+state bal: int = storage::get(sender);
+state to_bal: int = storage::get(to);
+
+constraint bal >= amount;
+constraint bal - bal' == amount;
+constraint to_bal' - to_bal == amount;
+
+solve satisfy;
+"#;
+
+    let deployed_intent = compile_yurt(code);
+
+    let mut server = Server::new();
+
+    let deployed_address = server
+        .deploy_intent_set(vec![deployed_intent.clone()])
+        .unwrap();
+
+    let code = r#"
+let eoa: int[4] = [0, 0, 0, 0];
+let to = 0x0000000000000000000000000000000000000000000000000000000000000001;
+state bal: int = storage::get_extern(${deployed_address}, eoa); // 42
+state to_bal: int = storage::get_extern(${deployed_address}, to); // 42
+
+constraint bal - bal' == amount;
+constraint to_bal' - to_bal == amount;
+
+solve satisfy;
+"#;
+    let code = code.replace(
+        "${deployed_address}",
+        &format!("0x{}", hex::encode(IntentAddress::from(deployed_address).0)),
+    );
+
+    let mut intent = compile_yurt(&code);
+    intent.slots.permits = 1;
+    let transient_address = intent.intent_address();
+
+    let transitions = [
+        SolutionData {
+            intent_to_solve: SourceAddress::transient(transient_address.clone()),
+            decision_variables: vec![],
+            sender: Sender::Eoa([0; 4]),
+        },
+        SolutionData {
+            intent_to_solve: SourceAddress::persistent(
+                deployed_address.into(),
+                deployed_intent.intent_address(),
+            ),
+            decision_variables: vec![0, 0, 0, 0, 0, 0, 0, 1, 10],
+            sender: Sender::transient([0; 4], transient_address),
+        },
+    ];
+
+    let solution = Solution {
+        data: transitions.into_iter().collect(),
+        state_mutations: vec![StateMutation {
+            address: deployed_address.into(),
+            mutations: vec![
+                Mutation::Key(KeyMutation {
+                    key: [0, 0, 0, 0],
+                    value: Some(90),
+                }),
+                Mutation::Key(KeyMutation {
+                    key: [0, 0, 0, 1],
+                    value: Some(10),
+                }),
+            ],
+        }],
+    };
+
+    let address: Address = deployed_address;
+    server.db().stage(address, [0, 0, 0, 0], Some(100));
+    server.db().commit();
+
+    server.submit_intent(intent).unwrap();
+    let utility = server.submit_solution(solution).unwrap();
+    assert_eq!(utility, 2);
+}
+
 fn compile_yurt(code: &str) -> Intent {
     let mut tmpfile = NamedTempFile::new().unwrap();
     write!(tmpfile.as_file_mut(), "{}", code).unwrap();
