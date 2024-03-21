52eedc1e1734163cf2f63f976caa74ea648c2bc5
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-03-21 16:11:15 +1100

    Use asm-gen to generate constraint-asm, state-asm decls

--- crates/constraint-asm/src/lib.rs
+++ crates/constraint-asm/src/lib.rs
@@ -1,154 +1,21 @@
+//! Assembly for checking constraints.
+//!
+//! # Op Table
+#![doc = essential_asm_gen::gen_constraint_ops_docs_table!()]
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
-//! # Assembly for checking constraints.
 
-use essential_types::Word;
-use serde::Deserialize;
-use serde::Serialize;
+#[doc(inline)]
+pub use op::{Constraint as Op, *};
+#[doc(inline)]
+pub use opcode::Constraint as Opcode;
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Set of operations that can be performed to check constraints.
-pub enum Op {
-    /// Push word onto stack.
-    Push(Word),
-    /// Pop word from stack.
-    Pop,
-    /// Duplicate top word on stack.
-    Dup,
-    /// Params -> index.
-    /// Duplicate word at index counting from the top of the stack and push it onto the stack.
-    DupFrom,
-    /// Swap top two words on stack.
-    Swap,
-    /// Operations for computing predicates.
-    Pred(Pred),
-    /// Operations for computing arithmetic and logic.
-    Alu(Alu),
-    /// Operations for accessing input data.
-    Access(Access),
-    /// Operations for performing cryptographic operations.
-    Crypto(Crypto),
+/// Typed representation of an operation its associated data.
+mod op {
+    essential_asm_gen::gen_constraint_op_decls!();
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Operations for accessing input data.
-pub enum Access {
-    /// params -> slot
-    /// return -> decision_word
-    ///
-    /// Slot must be in range or vm will panic.
-    DecisionVar,
-    /// params -> {slot, range}
-    /// return -> decision_words: list len range
-    ///
-    /// Slot..(Slot + Range) must be in range or vm will panic.
-    DecisionVarRange,
-    /// params -> {slot, delta: bool}
-    /// return -> state_word
-    ///
-    /// Slot must be in range or vm will panic.
-    /// Empty slots will be returned as 0.
-    /// Use StateIsSome to check if a slot is empty.
-    State,
-    /// params -> {slot, range, delta: bool}
-    /// return -> state_words: list len range
-    ///
-    /// Slot..(Slot + Range) must be in range or vm will panic.
-    /// Empty slots will be returned as 0.
-    /// Use StateIsSome to check if a slot is empty.
-    StateRange,
-    /// params -> {slot, delta: bool}
-    /// return -> is_some: bool
-    ///
-    /// Slot must be in range or vm will panic.
-    StateIsSome,
-    /// params -> {slot, range, delta: bool}
-    /// return -> is_somes: list of bools with len range
-    ///
-    /// Slot..(Slot + Range) must be in range or vm will panic.
-    StateIsSomeRange,
-    /// Params -> slot,
-    /// Return -> key: list len 4
-    ///
-    /// Returns the key that is being proposed for mutation at this slot.
-    MutKeys,
-    /// Return -> word
-    ///
-    /// Get the number of mutable keys being proposed for mutation.
-    MutKeysLen,
-    /// Return -> key: list len 4
-    ///
-    /// Get the content hash of this intent.
-    ThisAddress,
-    /// Return -> key: list len 4
-    ///
-    /// Get the content hash of the set of intents that this intent belongs to.
-    ThisSetAddress,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Operations for computing predicates.
-pub enum Pred {
-    /// params -> {lhs, rhs}
-    /// return -> bool
-    Eq,
-    /// params -> {lhs: list len 4, rhs: list len 4}
-    /// return -> bool
-    Eq4,
-    /// params -> {lhs, rhs}
-    /// return -> bool
-    Gt,
-    /// params -> {lhs, rhs}
-    /// return -> bool
-    Lt,
-    /// params -> {lhs, rhs}
-    /// return -> bool
-    Gte,
-    /// params -> {lhs, rhs}
-    /// return -> bool
-    Lte,
-    /// params -> {lhs, rhs}
-    /// return -> bool
-    And,
-    /// params -> {lhs, rhs}
-    /// return -> bool
-    Or,
-    /// params -> word
-    /// return -> bool
-    Not,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Operations for computing arithmetic and logic.
-pub enum Alu {
-    /// params -> {lhs, rhs}
-    /// return -> word
-    Add,
-    /// params -> {lhs, rhs}
-    /// return -> word
-    Sub,
-    /// params -> {lhs, rhs}
-    /// return -> word
-    Mul,
-    /// params -> {lhs, rhs}
-    /// return -> word
-    Div,
-    /// params -> {lhs, rhs}
-    /// return -> word
-    Mod,
-    /// Adds the offset to the hash.
-    /// params -> {hash: list len 4, offset}
-    /// return -> new_hash: list len 4
-    HashOffset,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Operations for performing cryptographic operations.
-pub enum Crypto {
-    /// params -> {data, data_len}
-    /// return -> hash: list len 4
-    Sha256,
-    /// params -> {data, data_len, signature: list len 8, public_key: list len 4}
-    /// return -> bool
-    VerifyEd25519,
+/// Typed representation of the opcode, without any associated data.
+pub mod opcode {
+    essential_asm_gen::gen_constraint_opcode_decls!();
 }
--- crates/state-asm/src/lib.rs
+++ crates/state-asm/src/lib.rs
@@ -1,109 +1,23 @@
-//! # Assembly for state read operations.
-
+//! Assembly for state read operations.
+//!
+//! # Op Table
+#![doc = essential_asm_gen::gen_ops_docs_table!()]
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 
-pub use essential_constraint_asm::{self as constraint_asm, Op};
-use serde::Deserialize;
-use serde::Serialize;
+#[doc(inline)]
+pub use op::{StateRead as Op, *};
+#[doc(inline)]
+pub use opcode::StateRead as Opcode;
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Set of operations that can be performed on the state.
-pub enum StateReadOp {
-    /// All the operations available to the constraint checker.
-    Constraint(Op),
-    /// Operations for reading state.
-    State(State),
-    /// Operations for controlling the flow of the program.
-    ControlFlow(ControlFlow),
-    /// Operations for controlling the memory.
-    Memory(Memory),
+/// Typed representation of an operation its associated data.
+mod op {
+    pub use essential_constraint_asm::{Access, Alu, Constraint, Crypto, Pred};
+    essential_asm_gen::gen_state_read_op_decls!();
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Control flow operations.
-pub enum ControlFlow {
-    /// End the execution of the program and return the keys and memory.
-    Halt,
-    /// Jump to the given address.
-    /// params -> address
-    Jump,
-    /// Jump to the given address if the value is true.
-    /// params -> {address, value: bool}
-    JumpIf,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Memory operations.
-pub enum Memory {
-    /// Allocate new memory to the end of the memory.
-    /// params -> size
-    Alloc,
-    /// Free the given size of memory from the end of the memory.
-    /// params -> size
-    Free,
-    /// Truncate the memory to the given size.
-    /// This does not effect the capacity of the memory.
-    /// params -> size
-    Truncate,
-    /// Load the index of memory onto the stack.
-    /// params -> index
-    /// return -> value
-    ///
-    /// Panics if the index is out of bounds.
-    /// Returns 0 if the value is None.
-    /// Use `IsSome` to check if the value is None.
-    Load,
-    /// Store the value at the index of memory.
-    /// params -> {index, value}
-    ///
-    /// Panics if the index is out of bounds.
-    Store,
-    /// Push the value onto the end of the memory.
-    /// params -> value
-    ///
-    /// Panics if not enough memory is allocated.
-    Push,
-    /// Similar to push except that it pushes a None value.
-    ///
-    /// Panics if not enough memory is allocated.
-    PushNone,
-    /// Set the value at the index of memory to None.
-    /// params -> index
-    ///
-    /// Panics if the index is out of bounds.
-    Clear,
-    /// Set a range of memory to None.
-    /// params -> {index, amount}
-    ///
-    /// Panics if the index..(index + amount) is out of bounds.
-    ClearRange,
-    /// Check if the value at the index of memory is Some.
-    /// params -> index
-    ///
-    /// Panics if the index is out of bounds.
-    IsSome,
-    /// Get the current capacity of the memory.
-    /// return -> capacity
-    Capacity,
-    /// Get the current length of the memory.
-    /// return -> length
-    Length,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-/// Operations for reading state.
-pub enum State {
-    /// Read a range of words from state starting at the key.
-    /// params -> {key: list len 4, amount}
-    /// return -> address in memory where the words are stored
-    ///
-    /// Panics if not enough memory is allocated.
-    StateReadWordRange,
-    /// Read a range of words from external state starting at the key.
-    /// params -> {extern_address: list len 4, key: list len 4, amount}
-    /// return -> address in memory where the words are stored
-    ///
-    /// Panics if not enough memory is allocated.
-    StateReadWordRangeExtern,
+/// Typed representation of the opcode, without any associated data.
+pub mod opcode {
+    pub use essential_constraint_asm::opcode::*;
+    essential_asm_gen::gen_state_read_opcode_decls!();
 }
