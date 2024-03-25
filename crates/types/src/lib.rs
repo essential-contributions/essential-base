54af572ab25ab2336937645768b17c5916bb0114
Tom <tomrgowan@gmail.com>
2024-03-25 15:41:05 +1100

    Various changes

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,13 +1,15 @@
 #![forbid(unsafe_code)]
-#![warn(missing_docs)]
+#![deny(missing_docs)]
 //! # Common types for Essential Chain.
 
-use std::ops::Range;
+use core::time::Duration;
 
 use serde::{Deserialize, Serialize};
+use solution::Solution;
 
 pub mod convert;
 pub mod intent;
+pub mod signature_ser;
 pub mod slots;
 pub mod solution;
 
@@ -23,16 +25,23 @@ pub type Word = i64;
 /// Key for state data.
 pub type Key = [Word; 4];
 
-/// Range of keys for state data.
-pub type KeyRange = Range<Key>;
-
 /// Hash encoded as a 32 byte array.
 pub type Hash = [u8; 32];
 
 /// Externally owned account.
-///
-/// Note this type will likely change in the future.
 pub type Eoa = [u8; 32];
+
+/// The signature over some data.
+pub type Signature = [u8; 64];
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+/// Owner of a state key.
+pub enum Owner {
+    /// An externally owned account.
+    Eoa(Eoa),
+    /// An intents account.
+    Intent(ContentAddress),
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 /// Content address of an intent or set of intents.
@@ -46,3 +55,35 @@ pub struct IntentAddress {
     /// Content address of the intent.
     pub intent: ContentAddress,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+/// A signed piece of data.
+pub struct Signed<T> {
+    /// The data that is signed.
+    pub data: T,
+    /// The signature over the data.
+    #[serde(with = "signature_ser")]
+    pub signature: Signature,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+/// A protocol block.
+pub struct Block {
+    /// The block number.
+    pub number: u64,
+    /// The timestamp of the block.
+    pub timestamp: Duration,
+    /// The batch of solutions.
+    pub batch: Batch,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+/// A batch of solutions
+pub struct Batch {
+    /// The solutions in the batch.
+    pub solutions: Vec<Signed<Solution>>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+/// The storage layout of a stateful intent.
+pub struct StorageLayout;
---
+++ crates/types/src/signature_ser.rs
@@ -0,0 +1,25 @@
+//! Helpers for serializing and deserializing `Signature` types.
+
+use serde::{Deserialize, Deserializer, Serializer};
+
+/// Serialize a `Signature` as a byte array.
+pub fn serialize<S>(value: &[u8; 64], serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_bytes(&value[..])
+}
+
+/// Deserialize a `Signature` from a byte array.
+pub fn deserialize<'de, D>(deserializer: D) -> Result<[u8; 64], D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let bytes = Vec::<u8>::deserialize(deserializer)?;
+    if bytes.len() != 64 {
+        return Err(serde::de::Error::custom("invalid length"));
+    }
+    let mut result = [0; 64];
+    result.copy_from_slice(&bytes);
+    Ok(result)
+}
