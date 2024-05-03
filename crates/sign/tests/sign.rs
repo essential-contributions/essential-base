d1cc8247ffab35350d44e64dd2a0d123322a3771
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-05-03 18:44:06 +1000

    feat: Introduce a minimal essential-sign crate
    This extracts the essential_server's `utils` signing related functions
    (sign, verify, recover) into a dedicated, minimal `essential-sign`
    crate.
    
    This is a minimal step in the process of refactoring essential-server's
    state transition verification logic into `essential-base`, #81.

---
+++ crates/sign/tests/sign.rs
@@ -0,0 +1,68 @@
+use essential_sign::sign;
+use essential_types::{
+    intent::{Directive, Intent},
+    slots::Slots,
+    Signature,
+};
+use rand::SeedableRng;
+use secp256k1::{PublicKey, Secp256k1, SecretKey};
+
+fn test_intent() -> Intent {
+    Intent {
+        slots: Slots {
+            decision_variables: 1,
+            state: Default::default(),
+        },
+        state_read: Default::default(),
+        constraints: Default::default(),
+        directive: Directive::Satisfy,
+    }
+}
+
+fn random_keypair(seed: [u8; 32]) -> (SecretKey, PublicKey) {
+    let mut rng = rand::rngs::SmallRng::from_seed(seed);
+    let secp = Secp256k1::new();
+    secp.generate_keypair(&mut rng)
+}
+
+#[test]
+fn sign_intent() {
+    let (sk, _pk) = random_keypair([0xcd; 32]);
+    let signed = sign(test_intent(), sk);
+    let expected_signature_hex = concat!(
+        "641965942f822f382fa5cad859c5906f2acaac4a611e7f8c66d6d1aecde79919",
+        "75aa2c9ae3b0d170c78c39acc49fecafca3c13e92c32c031af113eabdf973239"
+    );
+    let hex = hex::encode(signed.signature.0);
+    assert_eq!(expected_signature_hex, hex);
+}
+
+#[test]
+fn recover() {
+    let (sk, pk) = random_keypair([0xcd; 32]);
+    let data = test_intent();
+    let signed = sign(data, sk);
+    let recovered_pk = essential_sign::recover(signed).unwrap();
+    assert_eq!(pk, recovered_pk);
+}
+
+#[test]
+fn fail_to_recover() {
+    let (sk, _pk) = random_keypair([0xcd; 32]);
+    let data = test_intent();
+    let signed = sign(data, sk);
+    let mut corrupted_signed = signed.clone();
+    corrupted_signed.signature.1 = (corrupted_signed.signature.1 + 1) % 4;
+    assert!(essential_sign::recover(corrupted_signed).is_err());
+}
+
+#[test]
+fn verify_signature() {
+    let (sk, _pk) = random_keypair([0xcd; 32]);
+    let data = test_intent();
+    let signed = sign(data, sk);
+    let mut signed_corrupted = signed.clone();
+    signed_corrupted.signature = Signature([0u8; 64], 0);
+    assert!(essential_sign::verify(&signed));
+    assert!(!essential_sign::verify(&signed_corrupted));
+}
