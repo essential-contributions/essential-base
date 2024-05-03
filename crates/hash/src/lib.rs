ae90356ea4dd461ce7b207407f3e2076ac359aa0
mitchmindtree <mail@mitchellnordine.com>
2024-05-03 20:44:56 +1000

    feat: Introduce a minimal `essential-hash` crate (#82)
    This extracts the essential_server's `utils` hashing related functions
    into a dedicated, minimal `essential-hash` crate.
    
    This is a minimal step in the process of refactoring essential-server's
    state transition verification logic into `essential-base`, #81.

---
+++ crates/hash/src/lib.rs
@@ -0,0 +1,29 @@
+//! A minimal crate containing Essential's [`hash`] function and associated pre-hash
+//! generic serialization implementation [`serialize`] based on [`postcard`].
+
+#![deny(missing_docs)]
+#![deny(unsafe_code)]
+
+use essential_types::Hash;
+use serde::Serialize;
+use sha2::Digest;
+
+/// Serialize data for hashing using postcard.
+///
+/// This serialization format is standardized across essential crates.
+/// Attempting to hash data serialized with any other serialization
+/// implementation will almost certainly result in a different hash.
+pub fn serialize<T: Serialize>(t: &T) -> Vec<u8> {
+    postcard::to_allocvec(t).expect("`postcard`'s `Serializer` implementation should never fail")
+}
+
+/// Hash data using SHA-256.
+///
+/// Internally, this first serializes the given type using [`serialize`] then
+/// hashes the resulting slice of bytes using the `Sha256` digest.
+pub fn hash<T: Serialize>(t: &T) -> Hash {
+    let data = serialize(t);
+    let mut hasher = <sha2::Sha256 as sha2::Digest>::new();
+    hasher.update(&data);
+    hasher.finalize().into()
+}
---
+++ crates/hash/tests/hash.rs
@@ -0,0 +1,32 @@
+use essential_types::{
+    intent::{Directive, Intent},
+    slots::Slots,
+};
+
+fn test_intent() -> Intent {
+    Intent {
+        slots: Slots {
+            decision_variables: 1,
+            state: Default::default(),
+        },
+        state_read: Default::default(),
+        constraints: Default::default(),
+        directive: Directive::Satisfy,
+    }
+}
+
+#[test]
+fn serialize_intent() {
+    let serialization = essential_hash::serialize(&test_intent());
+    let hex = hex::encode(&serialization);
+    let expected_hex = "0100000000";
+    assert_eq!(hex, expected_hex);
+}
+
+#[test]
+fn hash_intent() {
+    let hash = essential_hash::hash(&test_intent());
+    let expected_hash_hex = "957b88b12730e646e0f33d3618b77dfa579e8231e3c59c7104be7165611c8027";
+    let hex = hex::encode(&hash);
+    assert_eq!(hex, expected_hash_hex);
+}
