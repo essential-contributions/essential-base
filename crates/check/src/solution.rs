93b38920d72d5c99a77bb755b6730a35a46e016e
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-05-03 17:24:48 +1000

    Separate out a `check_intent` fn. Re-add intent constraint checking.

--- crates/check/src/solution.rs
+++ crates/check/src/solution.rs
@@ -1,12 +1,13 @@
 //! Items related to validating `Solution`s.
 
 use crate::{
+    constraint_vm,
     state_read_vm::{
         self, Access, BytecodeMapped, Gas, GasLimit, SolutionAccess, StateRead, StateSlotSlice,
         StateSlots,
     },
     types::{
-        intent::Intent,
+        intent::{Directive, Intent},
         slots::{self, StateSlot},
         solution::{Solution, SolutionDataIndex},
         IntentAddress, Word,
@@ -18,8 +19,7 @@ use tokio::task::JoinSet;
 /// The utility score of a solution.
 pub type Utility = f64;
 
-/// Checks a solution against its associated intents.
-///
+/// Checks a solution against its associated intents, one task per solution data.
 ///
 /// For each of the solution's `data` elements, reads the pre and post state
 /// slots for the associated intent with access to the given `pre_state` and
@@ -34,7 +34,7 @@ pub type Utility = f64;
 ///   solution. Calls to `intent` must complete immediately. The necessary
 ///   intents are assumed to have been read from storage ahead of time.
 ///
-/// Returns the `utility` score of the solution alongside the total gas spent.
+/// Returns the utility score of the solution alongside the total gas spent.
 pub async fn check_intents<SA, SB>(
     pre_state: &SA,
     post_state: &SB,
@@ -50,91 +50,24 @@ where
     // Read pre and post states then check constraints.
     let mut set: JoinSet<anyhow::Result<_>> = JoinSet::new();
     for (solution_data_index, data) in solution.data.iter().enumerate() {
+        let solution_data_index: SolutionDataIndex = solution_data_index.try_into()?;
         let Some(intent) = get_intent(&data.intent_to_solve) else {
             anyhow::bail!("Intent in solution data not found in intents set");
         };
         let solution = solution.clone();
         let pre_state: SA = pre_state.clone();
         let post_state: SB = post_state.clone();
-        let solution_data_index: SolutionDataIndex = solution_data_index.try_into()?;
-
         set.spawn(async move {
-            // Get the length of state slots for this intent.
-            let intent_state_len: usize = slots::state_len(&intent.slots.state)
-                .ok_or(anyhow::anyhow!("State slots have no length"))?
-                .try_into()?;
-
-            let mut total_gas = 0;
-
-            // Initialize pre and post slots.
-            let mut pre_slots: Vec<Option<Word>> = vec![None; intent_state_len];
-            let mut post_slots: Vec<Option<Word>> = vec![None; intent_state_len];
-            let solution_access = SolutionAccess::new(&solution, solution_data_index);
-
-            // Read pre and post states.
-            for (state_read_index, state_read) in intent.state_read.iter().enumerate() {
-                let state_read_index: u16 = state_read_index.try_into()?;
-
-                // Map the bytecode ops ahead of execution to share the mapping
-                // between both pre and post state slot reads.
-                let state_read_mapped = BytecodeMapped::try_from(&state_read[..])?;
-
-                // Read pre state slots.
-                let (gas, new_pre_slots) = read_state(
-                    &state_read_mapped,
-                    Access {
-                        solution: solution_access,
-                        state_slots: StateSlots {
-                            pre: &pre_slots,
-                            post: &post_slots,
-                        },
-                    },
-                    &pre_state,
-                )
-                .await?;
-                total_gas += gas;
-
-                // Validate the `new_pre_slots`, then update `pre_slots`.
-                write_slots(
-                    state_read_index,
-                    &intent.slots.state,
-                    &mut pre_slots,
-                    &new_pre_slots,
-                )?;
-
-                // Read post state slots.
-                let (gas, new_post_slots) = read_state(
-                    &state_read_mapped,
-                    Access {
-                        solution: solution_access,
-                        state_slots: StateSlots {
-                            pre: &pre_slots,
-                            post: &post_slots,
-                        },
-                    },
-                    &post_state,
-                )
-                .await?;
-                total_gas += gas;
-                write_slots(
-                    state_read_index,
-                    &intent.slots.state,
-                    &mut post_slots,
-                    &new_post_slots,
-                )?;
-            }
-
-            // Check constraints.
-            let utility: Utility = todo!();
-            // let utility = check_constraints(
-            //     intent.clone(),
-            //     pre_slots,
-            //     post_slots,
-            //     solution,
-            //     solution_data_index,
-            // )
-            // .await?;
-            Ok((utility, total_gas))
+            let pre_state = pre_state;
+            let post_state = post_state;
+            check_intent(
+                &pre_state,
+                &post_state,
+                solution,
+                intent,
+                solution_data_index,
+            )
+            .await
         });
     }
 
@@ -158,16 +91,116 @@ where
     Ok((utility, total_gas))
 }
 
+/// Checks a solution against a single intent using the solution data at the given index.
+///
+/// Reads all pre and post state slots into memory, then checks all constraints.
+///
+/// ## Arguments
+///
+/// - `pre_state` must provide access to state *prior to* mutations being applied.
+/// - `post_state` must provide access to state *post* mutations being applied.
+/// - `solution_data_index` represents the data within `solution.data` that claims
+///   to solve this intent.
+///
+/// Returns the utility score of the solution alongside the total gas spent.
+pub async fn check_intent<SA, SB>(
+    pre_state: &SA,
+    post_state: &SB,
+    solution: Arc<Solution>,
+    intent: Arc<Intent>,
+    solution_data_index: SolutionDataIndex,
+) -> anyhow::Result<(Utility, Gas)>
+where
+    SA: StateRead + Sync,
+    SB: StateRead + Sync,
+{
+    // Get the length of state slots for this intent.
+    let intent_state_len: usize = slots::state_len(&intent.slots.state)
+        .ok_or(anyhow::anyhow!("State slots have no length"))?
+        .try_into()?;
+
+    // Track the total gas spent over all execution.
+    let mut total_gas = 0;
+
+    // Initialize pre and post slots. These will contain all state slots for all state reads.
+    let mut pre_slots: Vec<Option<Word>> = vec![None; intent_state_len];
+    let mut post_slots: Vec<Option<Word>> = vec![None; intent_state_len];
+    let solution_access = SolutionAccess::new(&solution, solution_data_index);
+
+    // Read pre and post states.
+    for (state_read_index, state_read) in intent.state_read.iter().enumerate() {
+        let state_read_index: u16 = state_read_index.try_into()?;
+
+        // Map the bytecode ops ahead of execution to share the mapping
+        // between both pre and post state slot reads.
+        let state_read_mapped = BytecodeMapped::try_from(&state_read[..])?;
+
+        // Read pre state slots and write them to the pre_slots slice.
+        let (gas, new_pre_slots) = read_state_slots(
+            &state_read_mapped,
+            Access {
+                solution: solution_access,
+                state_slots: StateSlots {
+                    pre: &pre_slots,
+                    post: &post_slots,
+                },
+            },
+            pre_state,
+        )
+        .await?;
+        total_gas += gas;
+        write_state_slots(
+            state_read_index,
+            &intent.slots.state,
+            &mut pre_slots,
+            &new_pre_slots,
+        )?;
+
+        // Read post state slots and write them to the post_slots slice.
+        let (gas, new_post_slots) = read_state_slots(
+            &state_read_mapped,
+            Access {
+                solution: solution_access,
+                state_slots: StateSlots {
+                    pre: &pre_slots,
+                    post: &post_slots,
+                },
+            },
+            post_state,
+        )
+        .await?;
+        total_gas += gas;
+        write_state_slots(
+            state_read_index,
+            &intent.slots.state,
+            &mut post_slots,
+            &new_post_slots,
+        )?;
+    }
+
+    // Check constraints.
+    let utility = check_intent_constraints(
+        solution,
+        solution_data_index,
+        intent.clone(),
+        Arc::from(pre_slots.into_boxed_slice()),
+        Arc::from(post_slots.into_boxed_slice()),
+    )
+    .await?;
+
+    Ok((utility, total_gas))
+}
+
 /// Reads state slots from storage using the given bytecode.
 ///
 /// The result is written to VM's memory.
 ///
 /// Returns the gas spent alongside the state slots consumed from the VM's memory.
-async fn read_state<S>(
+async fn read_state_slots<S>(
     bytecode_mapped: &BytecodeMapped<&[u8]>,
     access: Access<'_>,
     state_read: &S,
-) -> anyhow::Result<(Gas, Vec<Option<Word>>)>
+) -> anyhow::Result<(Gas, Box<StateSlotSlice>)>
 where
     S: StateRead,
 {
@@ -186,11 +219,11 @@ where
         .await
         .map_err(|e| anyhow::anyhow!("State read VM execution failed: {e}"))?;
 
-    Ok((gas_spent, vm.into_state_slots()))
+    Ok((gas_spent, vm.into_state_slots().into_boxed_slice()))
 }
 
 /// Write to the correct slots based on the state read index.
-fn write_slots(
+fn write_state_slots(
     state_read_index: u16,
     state_slots: &[StateSlot],
     slots: &mut StateSlotSlice,
@@ -222,4 +255,178 @@ fn write_slots(
     slots.copy_from_slice(output_slots);
 
     Ok(())
+}
+
+/// Checks if the given solution data at the given index satisfies the
+/// constraints of the given intent.
+///
+/// Returns the utility of the solution for the given intent.
+pub async fn check_intent_constraints(
+    solution: Arc<Solution>,
+    solution_data_index: SolutionDataIndex,
+    intent: Arc<Intent>,
+    pre_slots: Arc<StateSlotSlice>,
+    post_slots: Arc<StateSlotSlice>,
+) -> anyhow::Result<Utility> {
+    check_intent_constraints_parallel(
+        solution.clone(),
+        solution_data_index,
+        intent.clone(),
+        pre_slots.clone(),
+        post_slots.clone(),
+    )
+    .await
+    .map_err(|e| anyhow::anyhow!("Constraint VM execution failed: {e}"))?;
+    calculate_utility(
+        solution,
+        solution_data_index,
+        intent.clone(),
+        pre_slots,
+        post_slots,
+    )
+    .await
+}
+
+/// Check intents in parallel without sleeping any threads.
+async fn check_intent_constraints_parallel(
+    solution: Arc<Solution>,
+    solution_data_index: SolutionDataIndex,
+    intent: Arc<Intent>,
+    pre_slots: Arc<StateSlotSlice>,
+    post_slots: Arc<StateSlotSlice>,
+) -> anyhow::Result<()> {
+    let mut handles = Vec::with_capacity(intent.constraints.len());
+
+    // Spawn each constraint onto a rayon thread and
+    // check them in parallel.
+    for i in 0..intent.constraints.len() {
+        let (tx, rx) = tokio::sync::oneshot::channel();
+        handles.push(rx);
+
+        // These are all cheap Arc clones.
+        let solution = solution.clone();
+        let pre_slots = pre_slots.clone();
+        let post_slots = post_slots.clone();
+        let intent = intent.clone();
+
+        // Spawn this sync code onto a rayon thread.
+        // This is a non-blocking operation.
+        rayon::spawn(move || {
+            let solution_access = SolutionAccess::new(&solution, solution_data_index);
+            let access = Access {
+                solution: solution_access,
+                state_slots: StateSlots {
+                    pre: &pre_slots,
+                    post: &post_slots,
+                },
+            };
+            let res = constraint_vm::eval_bytecode_iter(
+                intent
+                    .constraints
+                    .get(i)
+                    .expect("Safe due to above len check")
+                    .iter()
+                    .copied(),
+                access,
+            );
+            // Send the result back to the main thread.
+            // Send errors are ignored as if the recv is gone there's no one to send to.
+            let _ = tx.send((i, res));
+        })
+    }
+
+    // There's no way to know the size of these.
+    let mut failed = Vec::new();
+    let mut unsatisfied = Vec::new();
+
+    // Wait for all constraints to finish.
+    // The order of waiting on handles is not important as all
+    // constraints make progress independently.
+    for handle in handles {
+        // Get the index and result from the handle.
+        let (i, res): (usize, Result<bool, _>) = handle.await?;
+        match res {
+            // If the constraint failed, add it to the failed list.
+            Err(err) => failed.push((i, err)),
+            // If the constraint was unsatisfied, add it to the unsatisfied list.
+            Ok(b) if !b => unsatisfied.push(i),
+            // Otherwise, the constraint was satisfied.
+            _ => (),
+        }
+    }
+
+    // If there are any failed constraints, return an error.
+    if !failed.is_empty() {
+        return Err(essential_constraint_vm::error::ConstraintErrors(failed).into());
+    }
+
+    // If there are any unsatisfied constraints, return an error.
+    if !unsatisfied.is_empty() {
+        return Err(essential_constraint_vm::error::ConstraintsUnsatisfied(unsatisfied).into());
+    }
+    Ok(())
+}
+
+/// Calculates utility of solution for intent.
+///
+/// Returns utility.
+async fn calculate_utility(
+    solution: Arc<Solution>,
+    solution_data_index: SolutionDataIndex,
+    intent: Arc<Intent>,
+    pre_slots: Arc<StateSlotSlice>,
+    post_slots: Arc<StateSlotSlice>,
+) -> anyhow::Result<f64> {
+    match &intent.directive {
+        Directive::Satisfy => Ok(1.0),
+        Directive::Maximize(_) | Directive::Minimize(_) => {
+            // Spawn this sync code onto a rayon thread.
+            let (tx, rx) = tokio::sync::oneshot::channel();
+            rayon::spawn(move || {
+                let solution_access = SolutionAccess::new(&solution, solution_data_index);
+                let access = Access {
+                    solution: solution_access,
+                    state_slots: StateSlots {
+                        pre: &pre_slots,
+                        post: &post_slots,
+                    },
+                };
+                // Extract the directive code.
+                let code = match intent.directive {
+                    Directive::Maximize(ref code) | Directive::Minimize(ref code) => code,
+                    _ => unreachable!("As this is already checked above"),
+                };
+
+                // Execute the directive code.
+                match constraint_vm::exec_bytecode_iter(code.iter().copied(), access) {
+                    Ok(mut stack) => match stack.pop3() {
+                        Ok([start, end, value]) => {
+                            // Return the normalized value back to the main thread.
+                            // Send errors are ignored as if the recv is dropped.
+                            let _ = tx.send(normalize_utility(value, start, end));
+                        }
+                        Err(e) => {
+                            // Return the error back to the main thread.
+                            // Send errors are ignored as if the recv is dropped.
+                            let _ = tx.send(Err(e.into()));
+                        }
+                    },
+                    Err(e) => {
+                        // Return the error back to the main thread.
+                        // Send errors are ignored as if the recv is dropped.
+                        let _ = tx.send(Err(e.into()));
+                    }
+                }
+            });
+
+            // Await the result of the utility calculation.
+            rx.await?
+        }
+    }
+}
+
+fn normalize_utility(value: Word, start: Word, end: Word) -> anyhow::Result<Utility> {
+    anyhow::ensure!(start < end, "Invalid range for directive");
+    let normalized = (value - start) as f64 / (end - start) as f64;
+    Ok(normalized.clamp(0.0, 1.0))
 }
