a771e94ec5358d0f2a7734fa935e5d87817730fd
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-05-06 11:37:21 +1000

    Rename intent errors to invalid, better reflecting validity check

--- crates/check/tests/intent.rs
+++ crates/check/tests/intent.rs
@@ -33,6 +33,38 @@ fn random_keypair(seed: [u8; 32]) -> (SecretKey, PublicKey) {
 }
 
 #[test]
+fn signed_set_one_empty_intent() {
+    let intent = empty_intent();
+    let (sk, _pk) = random_keypair([0; 32]);
+    let signed = sign(vec![intent], sk);
+    intent::check_signed_set(&signed).unwrap();
+}
+
+#[test]
+fn invalid_signature() {
+    let intent = empty_intent();
+    let (sk, _pk) = random_keypair([0; 32]);
+    let mut signed = sign(vec![intent], sk);
+    signed.signature.0 = [0; 64];
+    assert!(matches!(
+        intent::check_signed_set(&signed).unwrap_err(),
+        intent::InvalidSignedSet::Signature(_),
+    ));
+}
+
+#[test]
+fn too_many_intents() {
+    let intents: Vec<Intent> = vec![empty_intent(); intent::MAX_INTENTS + 1];
+    let (sk, _pk) = random_keypair([0; 32]);
+    let signed = sign(intents, sk);
+    assert!(matches!(
+        intent::check_signed_set(&signed).unwrap_err(),
+        intent::InvalidSignedSet::Set(intent::InvalidSet::TooManyIntents(n))
+            if n == intent::MAX_INTENTS + 1
+    ));
+}
+
+#[test]
 fn too_many_decision_variables() {
     let mut intent = empty_intent();
     intent.slots = Slots {
@@ -41,7 +73,7 @@ fn too_many_decision_variables() {
     };
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::Slots(intent::SlotsError::TooManyDecisionVariables(n))
+        intent::InvalidIntent::Slots(intent::InvalidSlots::TooManyDecisionVariables(n))
             if n == intent::MAX_DECISION_VARIABLES + 1
     ));
 }
@@ -55,7 +87,7 @@ fn too_many_state_slots() {
     };
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::Slots(intent::SlotsError::TooManyStateSlots(n))
+        intent::InvalidIntent::Slots(intent::InvalidSlots::TooManyStateSlots(n))
             if n == intent::MAX_NUM_STATE_SLOTS + 1
     ));
 }
@@ -73,7 +105,7 @@ fn state_slot_length_exceeds_limit() {
     };
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::Slots(intent::SlotsError::StateSlotLengthExceedsLimit(None)),
+        intent::InvalidIntent::Slots(intent::InvalidSlots::StateSlotLengthExceedsLimit(None)),
     ));
 }
 
@@ -90,40 +122,8 @@ fn test_fail_state_slots_length_too_large() {
     };
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::Slots(intent::SlotsError::StateSlotLengthExceedsLimit(Some(n)))
+        intent::InvalidIntent::Slots(intent::InvalidSlots::StateSlotLengthExceedsLimit(Some(n)))
             if n == intent::MAX_STATE_LEN + 1,
-    ));
-}
-
-#[test]
-fn signed_set_one_empty_intent() {
-    let intent = empty_intent();
-    let (sk, _pk) = random_keypair([0; 32]);
-    let signed = sign(vec![intent], sk);
-    intent::check_signed_set(&signed).unwrap();
-}
-
-#[test]
-fn invalid_signature() {
-    let intent = empty_intent();
-    let (sk, _pk) = random_keypair([0; 32]);
-    let mut signed = sign(vec![intent], sk);
-    signed.signature.0 = [0; 64];
-    assert!(matches!(
-        intent::check_signed_set(&signed).unwrap_err(),
-        intent::SignedSetError::InvalidSignature(_),
-    ));
-}
-
-#[test]
-fn too_many_intents() {
-    let intents: Vec<Intent> = vec![empty_intent(); intent::MAX_INTENTS + 1];
-    let (sk, _pk) = random_keypair([0; 32]);
-    let signed = sign(intents, sk);
-    assert!(matches!(
-        intent::check_signed_set(&signed).unwrap_err(),
-        intent::SignedSetError::InvalidSet(intent::SetError::TooManyIntents(n))
-            if n == intent::MAX_INTENTS + 1
     ));
 }
 
@@ -133,7 +133,7 @@ fn directive_too_large() {
     intent.directive = Directive::Maximize(vec![0; intent::MAX_DIRECTIVE_SIZE + 1]);
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::Directive(intent::DirectiveError::TooManyBytes(n))
+        intent::InvalidIntent::Directive(intent::InvalidDirective::TooManyBytes(n))
             if n == intent::MAX_DIRECTIVE_SIZE + 1
     ));
 }
@@ -144,7 +144,7 @@ fn too_many_state_reads() {
     intent.state_read = vec![vec![]; intent::MAX_STATE_READS + 1];
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::StateReads(intent::StateReadsError::TooMany(n))
+        intent::InvalidIntent::StateReads(intent::InvalidStateReads::TooMany(n))
             if n == intent::MAX_STATE_READS + 1
     ));
 }
@@ -155,29 +155,29 @@ fn state_read_too_large() {
     intent.state_read = vec![vec![0u8; intent::MAX_STATE_READ_SIZE_IN_BYTES + 1]];
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::StateReads(intent::StateReadsError::StateRead(0, intent::StateReadError::TooManyBytes(n)))
+        intent::InvalidIntent::StateReads(intent::InvalidStateReads::StateRead(0, intent::InvalidStateRead::TooManyBytes(n)))
             if n == intent::MAX_STATE_READ_SIZE_IN_BYTES + 1
     ));
 }
 
 #[test]
-fn test_fail_too_many_constraints() {
+fn too_many_constraints() {
     let mut intent = empty_intent();
     intent.constraints = vec![vec![]; intent::MAX_CONSTRAINTS + 1];
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::Constraints(intent::ConstraintsError::TooManyConstraints(n))
+        intent::InvalidIntent::Constraints(intent::InvalidConstraints::TooManyConstraints(n))
             if n == intent::MAX_CONSTRAINTS + 1
     ));
 }
 
 #[test]
-fn test_fail_constraint_too_large() {
+fn constraint_too_large() {
     let mut intent = empty_intent();
     intent.constraints = vec![vec![0u8; intent::MAX_CONSTRAINT_SIZE_IN_BYTES + 1]];
     assert!(matches!(
         intent::check(&intent).unwrap_err(),
-        intent::IntentError::Constraints(intent::ConstraintsError::Constraint(0, intent::ConstraintError::TooManyBytes(n)))
+        intent::InvalidIntent::Constraints(intent::InvalidConstraints::Constraint(0, intent::InvalidConstraint::TooManyBytes(n)))
             if n == intent::MAX_CONSTRAINT_SIZE_IN_BYTES + 1
     ));
 }
