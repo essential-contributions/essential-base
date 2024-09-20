4a44c93804fe9a336c62ea6dae6abb3fb1f751dc
Tom <tomrgowan@gmail.com>
2024-09-20 17:44:09 +1000

    feat: PanicIf op

--- crates/constraint-vm/src/total_control_flow/tests.rs
+++ crates/constraint-vm/src/total_control_flow/tests.rs
@@ -1,4 +1,9 @@
-use crate::{asm, exec_ops, test_util::test_access};
+use crate::{
+    asm,
+    error::{OpError, TotalControlFlowError},
+    exec_ops,
+    test_util::test_access,
+};
 
 #[test]
 fn test_jump_if() {
@@ -60,4 +65,46 @@ fn test_halt_if() {
     ];
     let stack = exec_ops(ops, access).unwrap();
     assert_eq!(&stack[..], &[3]);
+}
+
+#[test]
+fn test_panic_if() {
+    let mut stack = crate::Stack::default();
+    stack.push(42).unwrap();
+    stack.push(43).unwrap();
+    stack.push(1).unwrap();
+
+    let err = super::panic_if(&mut stack).unwrap_err();
+    assert!(err.to_string().ends_with("[42, 43]"),);
+    assert!(
+        matches!(err, OpError::TotalControlFlow(TotalControlFlowError::Panic(s)) if s == vec![42, 43])
+    );
+
+    let mut stack = crate::Stack::default();
+    stack.push(1).unwrap();
+
+    let err = super::panic_if(&mut stack).unwrap_err();
+    assert!(err.to_string().ends_with("[]"),);
+    assert!(
+        matches!(err, OpError::TotalControlFlow(TotalControlFlowError::Panic(s)) if s.is_empty())
+    );
+
+    let mut stack = crate::Stack::default();
+    stack.push(42).unwrap();
+    stack.push(43).unwrap();
+    stack.push(0).unwrap();
+
+    super::panic_if(&mut stack).unwrap();
+    assert_eq!(stack.len(), 2);
+    assert_eq!(stack.pop().unwrap(), 43);
+    assert_eq!(stack.pop().unwrap(), 42);
+
+    let mut stack = crate::Stack::default();
+    stack.push(42).unwrap();
+    stack.push(43).unwrap();
+    let err = super::panic_if(&mut stack).unwrap_err();
+    assert!(matches!(
+        err,
+        OpError::TotalControlFlow(TotalControlFlowError::InvalidPanicIfCondition)
+    ));
 }
