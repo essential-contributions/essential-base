e8c94d49573212b61aabf2ea37c5a2b8cfa97eb4
Bikem <bengisuozaydin@gmail.com>
2024-11-01 09:09:14 +1100

    clippy

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -78,6 +78,6 @@ pub struct Block {
 
 impl core::fmt::Debug for ContentAddress {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{}", hex::encode(&self.0))
+        write!(f, "{}", hex::encode(self.0))
     }
 }
