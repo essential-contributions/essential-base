38e70429936ef68831c941effcc44f480cf86811
Nick Furfaro <nfurfaro33@gmail.com>
2025-03-13 12:03:52 -0600

    test: (types) adjust to accommodate Read removal
    <!-- ps-id: 34f8abda-e7a5-4bfc-927a-af63a8cb85dc -->

--- crates/types/src/predicate/encode/tests.rs
+++ crates/types/src/predicate/encode/tests.rs
@@ -7,27 +7,22 @@ fn test_encode_predicate() {
             Node {
                 edge_start: 0,
                 program_address: ContentAddress([0; 32]),
-                reads: Reads::default(),
             },
             Node {
                 edge_start: u16::MAX,
                 program_address: ContentAddress([1; 32]),
-                reads: Reads::default(),
             },
             Node {
                 edge_start: 2,
                 program_address: ContentAddress([2; 32]),
-                reads: Reads::Post,
             },
             Node {
                 edge_start: u16::MAX,
                 program_address: ContentAddress([3; 32]),
-                reads: Reads::default(),
             },
             Node {
                 edge_start: u16::MAX,
                 program_address: ContentAddress([4; 32]),
-                reads: Reads::default(),
             },
         ],
         edges: vec![1, 2, 3, 4],
@@ -38,34 +33,19 @@ fn test_encode_predicate() {
         // node 0
         0u16.to_be_bytes().to_vec(), // edge_start
         vec![0; 32],                 // program_address
-        vec![
-            0u8, // reads
-        ],
         // node 1
         u16::MAX.to_be_bytes().to_vec(), // edge_start
         vec![1; 32],                     // program_address
-        vec![
-            0, // reads
-        ],
         // node 2
         2u16.to_be_bytes().to_vec(), // edge_start
         vec![2; 32],                 // program_address
-        vec![
-            1, // reads
-        ],
         // node 3
         u16::MAX.to_be_bytes().to_vec(), // edge_start
         vec![3; 32],                     // program_address
-        vec![
-            0, // reads
-        ],
         // node 4
         u16::MAX.to_be_bytes().to_vec(), // edge_start
         vec![4; 32],                     // program_address
-        vec![
-            0, // reads
-        ],
-        4u16.to_be_bytes().to_vec(), // len of edges
+        4u16.to_be_bytes().to_vec(),     // len of edges
         [1u16, 2, 3, 4]
             .into_iter()
             .flat_map(|x| x.to_be_bytes())
