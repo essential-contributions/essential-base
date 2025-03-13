42622075f27e5c637669f5fbd4ee2c6b2710ceb9
Tom <tomrgowan@gmail.com>
2025-03-14 10:42:01 +1100

    Adds encoding and decoding for mutations and lists of mutations to and from words. Layout of a mutation is key_len, key..., value_len, value... Layout of a list of mutations is num_mutations, mutation 1, mutation 2, ... closes #278
    <!-- ps-id: 6c72cbcb-353b-4394-8910-ec62285f2c9e -->

---
+++ crates/types/src/solution/decode.rs
@@ -0,0 +1,79 @@
+//! # Decoding
+//! Decoding for solution types.
+
+use crate::Word;
+
+use super::Mutation;
+
+#[cfg(test)]
+mod tests;
+
+/// Errors that can occur when decoding a predicate.
+#[derive(Debug, PartialEq)]
+pub enum MutationDecodeError {
+    /// The words are too short for the lengths of the key or value.
+    WordsTooShort,
+}
+
+impl std::error::Error for MutationDecodeError {}
+
+/// Decode a mutation from words.
+///
+/// # Layout
+/// ```text
+/// +-----------------+-----------------+
+/// | key length      | key             |
+/// +-----------------+-----------------+
+/// | value length    | value           |
+/// +-----------------+-----------------+
+/// ```
+pub fn decode_mutation(bytes: &[Word]) -> Result<Mutation, MutationDecodeError> {
+    if bytes.len() < 2 {
+        return Err(MutationDecodeError::WordsTooShort);
+    }
+    // Saturating cast
+    let key_len: usize = bytes[0].try_into().unwrap_or(usize::MAX);
+    if bytes.len() < 1 + key_len {
+        return Err(MutationDecodeError::WordsTooShort);
+    }
+    let key = bytes[1..1 + key_len].to_vec();
+    // Saturating cast
+    let value_len: usize = bytes[1 + key_len].try_into().unwrap_or(usize::MAX);
+
+    if bytes.len() < 2 + key_len + value_len {
+        return Err(MutationDecodeError::WordsTooShort);
+    }
+    let value = bytes[2 + key_len..2 + key_len + value_len].to_vec();
+    Ok(Mutation { key, value })
+}
+
+/// Decode a slice of mutations from words.
+///
+/// # Layout
+/// ```text
+/// +-----------------+-----------------+-----------------+-----------------+
+/// | num mutations   | mutation 1      | mutation 2      | ...             |
+/// +-----------------+-----------------+-----------------+-----------------+
+/// ```
+pub fn decode_mutations(bytes: &[Word]) -> Result<Vec<Mutation>, MutationDecodeError> {
+    if bytes.is_empty() {
+        return Err(MutationDecodeError::WordsTooShort);
+    }
+    // Saturating cast
+    let len: usize = bytes[0].try_into().unwrap_or(usize::MAX);
+    let mut mutations = Vec::with_capacity(len);
+    if len == 0 {
+        return Ok(mutations);
+    }
+    let mut i = 1;
+    while i < bytes.len() {
+        let Some(b) = bytes.get(i..) else {
+            return Err(MutationDecodeError::WordsTooShort);
+        };
+        let mutation = decode_mutation(b)?;
+        let size = mutation.encode_size();
+        i += size;
+        mutations.push(mutation);
+    }
+    Ok(mutations)
+}
