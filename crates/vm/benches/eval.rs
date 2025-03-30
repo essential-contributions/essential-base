be68c85689f4f58f51d03aa8361e1d5b7d76a053
Tom <tomrgowan@gmail.com>
2025-03-31 09:41:47 +1100

    Fix benchmarks
    <!-- ps-id: a394888c-cb1f-443d-a4ac-98a9ed68c1bf -->

--- crates/vm/benches/eval.rs
+++ crates/vm/benches/eval.rs
@@ -1,17 +1,16 @@
-use std::collections::HashSet;
+use std::sync::Arc;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use essential_asm as asm;
 use essential_asm::Op;
 use essential_types::{ContentAddress, PredicateAddress, Solution, SolutionSet};
-use essential_vm::{Access, GasLimit, Vm};
+use essential_vm::{bytecode::BytecodeMapped, Access, GasLimit, Vm};
 
 #[path = "../tests/util.rs"]
 mod util;
 use util::State;
 
 pub fn bench(c: &mut Criterion) {
-    let mutable_keys = HashSet::with_capacity(0);
     let solution_set = SolutionSet {
         solutions: vec![Solution {
             predicate_to_solve: PredicateAddress {
@@ -22,8 +21,9 @@ pub fn bench(c: &mut Criterion) {
             state_mutations: vec![],
         }],
     };
+    let solutions = Arc::new(solution_set.solutions.clone());
 
-    let access = Access::new(&solution_set, 0, &mutable_keys);
+    let access = Access::new(solutions, 0);
 
     let bytes = [asm::Stack::Push(1).into(), asm::Stack::Pop.into()];
     let mut vm = Vm::default();
@@ -31,12 +31,13 @@ pub fn bench(c: &mut Criterion) {
         let mut bytes: Vec<Op> = bytes.iter().cycle().take(i).copied().collect();
         bytes.push(asm::Stack::Push(1).into());
         let bytes: Vec<_> = asm::to_bytes(bytes).collect();
+        let bytecode = BytecodeMapped::try_from(&bytes[..]).unwrap();
         let op_gas_cost = &|_: &Op| 1;
         c.bench_function(&format!("push_pop_{}", i), |b| {
             b.iter(|| {
-                vm.exec_bytecode_iter(
-                    bytes.iter().copied(),
-                    access,
+                vm.exec_bytecode(
+                    &bytecode,
+                    access.clone(),
                     &State::EMPTY,
                     op_gas_cost,
                     GasLimit::UNLIMITED,
